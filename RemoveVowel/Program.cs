acb61c32eb94ef9a38a7dbb8d0f11c23ef19d73f
46790 <46790@LAPTOP-M22C1HP9>
2020-11-06 17:47:30 +0100

    remove vowel

---
+++ RemoveVowel/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace RemoveVowel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string finalString = RemoveVowel("leetcodeisacommunityforcoders");
+            Console.WriteLine(finalString);
+        }
+
+        public static string RemoveVowel(string s)
+        {
+
+            string[] notAllowedChars = new string[] {"a", "e", "i", "o", "u"};
+            if (s.Length >= 1 && s.Length <= 1000)
+            {
+                foreach (var a in notAllowedChars)
+                {
+                   s = s.Replace(a, string.Empty);
+                }
+            }
+            else
+            {
+                Console.WriteLine("non valid string");
+            }
+            return s;
+        }
+    }
+}
