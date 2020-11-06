6859f6181e62785455b4bf70ceec127483f76c72
46790 <46790@LAPTOP-M22C1HP9>
2020-11-06 15:12:14 +0100

    first commit

---
+++ LeetCodeProblems/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCodeProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 3, 1, 2, 10, 1 };
+            int[] test = RunningSum(numbers);
+
+            foreach (var n in test)
+            {
+                Console.WriteLine(n);
+            }
+
+        }
+        #region MyRegion
+
+        //public static int[] RunningSum(int[] nums)
+        //{
+        //    for (int i = 1; i < nums.Length; i++)
+        //    {
+        //       nums[i] = nums[i - 1] + nums[i];
+        //    }
+        //    return nums;
+        //}
+        #endregion
+    }
+
+
+}
