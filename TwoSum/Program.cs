41b44b5c6a60b89506b58896441b0da5b9ad5a2f
46790 <46790@LAPTOP-M22C1HP9>
2020-11-13 19:09:08 +0100

    integers to roman numerals

--- TwoSum/Program.cs
+++ TwoSum/Program.cs
@@ -8,8 +8,8 @@ namespace TwoSum
     {
         static void Main(string[] args)
         {
-            var song = SongDecoder("WUBWUBABCWUB");
-            Console.WriteLine(song);
+            var a = Solution(4);
+            Console.WriteLine(a);
         }
         #region TwoSum
         //public static int[] TwoSum(int[] numArray, int target)
@@ -124,7 +124,6 @@ namespace TwoSum
             }
         }
         #endregion
-
         #region songDecoder 
 
         public static string SongDecoder(string input)
@@ -136,5 +135,42 @@ namespace TwoSum
         }
 
         #endregion
+
+        /**
+         *Create a function taking a positive integer as its parameter and returning a string containing the Roman Numeral representation of that integer.
+         * Modern Roman numerals are written by expressing each digit separately starting with the left most digit and skipping any digit with a value of zero.
+         * In Roman numerals 1990 is rendered: 1000=M, 900=CM, 90=XC; resulting in MCMXC. 2008 is written as 2000=MM, 8=VIII; or MMVIII. 1666 uses each Roman symbol in descending order: MDCLXVI.
+         * Example:
+         * RomanConvert.Solution(1000) -- should return "M"
+         * Help:
+         * Symbol    Value
+         * I          1
+         * V          5
+         * X          10
+         * L          50
+         * C          100
+         * D          500
+         * M          1,000
+         * Remember that there can't be more than 3 identical symbols in a row.
+         * More about roman numerals - http://en.wikipedia.org/wiki/Roman_numerals
+         */
+
+
+
+        public static string Solution(int number)
+        {
+            List<string> romanNumerals = new List<string>() { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+            List<int> numerals = new List<int>() { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+            var romanNumeral = string.Empty;
+            while (number > 0)
+            {
+                var index = numerals.FindIndex(x => x <= number);
+
+                romanNumeral += romanNumerals[index];
+                number -= numerals[index];
+            }
+            return romanNumeral;
+        }
+
     }
 }
