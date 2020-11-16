43c32555542b4dca3c2b478eee7372a593060f54
46790 <46790@LAPTOP-M22C1HP9>
2020-11-16 17:18:31 +0100

    two sum

--- TwoSum/Program.cs
+++ TwoSum/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+ 
 
 namespace TwoSum
 {
@@ -8,74 +9,77 @@ namespace TwoSum
     {
         static void Main(string[] args)
         {
-            var a = Solution(4);
-            Console.WriteLine(a);
+            var a = TwoSum(new []{ 1,2,3 }, 4);
+            Console.WriteLine(a[0]);
+            Console.WriteLine(a[1]);
+            
         }
-        #region TwoSum
+            #region TwoSum
         //public static int[] TwoSum(int[] numArray, int target)
         //{
-        //    Dictionary<int, int> previousNumber = new Dictionary<int, int>();
-        //    for (int i = 0; i < numArray.Length; i++)
-        //    {
-        //        var currentNumberInArray = numArray[i];
-        //        var complement = target - currentNumberInArray;
-        //        if (previousNumber.ContainsKey(complement))
-        //        {
-        //            return new[] { previousNumber[complement], i };
-        //        }
-        //        previousNumber.Add(numArray[i], i);
+//        Dictionary<int, int> previousNumber = new Dictionary<int, int>();
+//            for (int i = 0; i<numArray.Length; i++)
+//            {
+//                var currentNumberInArray = numArray[i];
+//        var complement = target - currentNumberInArray;
+//                if (previousNumber.ContainsKey(complement))
+//                {
+//                    return new[] { previousNumber[complement], i
+//    };
+//}
+//previousNumber.Add(numArray[i], i);
 
-        //    }
-        //    throw new Exception("Invalid input");
-        //}
+//            }
+//            throw new Exception("Invalid input");
+//}
 
-        #endregion
-        #region CountTheDuplicates
-        //    Count the number of Duplicates 
-        //    Write a function that will return the count of distinct case-insensitive alphabetic characters
-        //    and numeric digits that occur more than once in the input string. The input string can be 
-        //    assumed to contain only alphabets(both uppercase and lowercase) and numeric digits.
+#endregion
+         #region CountTheDuplicates
+    //    Count the number of Duplicates 
+    //    Write a function that will return the count of distinct case-insensitive alphabetic characters
+    //    and numeric digits that occur more than once in the input string. The input string can be 
+    //    assumed to contain only alphabets(both uppercase and lowercase) and numeric digits.
 
-        //  Example
-        //"abcde" -> 0 # no characters repeats more than once
-        //"aabbcde" -> 2 # 'a' and 'b'
-        //"aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
-        //"indivisibility" -> 1 # 'i' occurs six times
-        //"Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
-        //"aA11" -> 2 # 'a' and '1'
-        //"ABBA" -> 2 # 'A' and 'B' each occur twice
+    //  Example
+    //"abcde" -> 0 # no characters repeats more than once
+    //"aabbcde" -> 2 # 'a' and 'b'
+    //"aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+    //"indivisibility" -> 1 # 'i' occurs six times
+    //"Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+    //"aA11" -> 2 # 'a' and '1'
+    //"ABBA" -> 2 # 'A' and 'B' each occur twice
 
 
-        //public static int CountTheDuplicates(string str)
-        //{
-        //    Dictionary<char, int> duplicateCounterDictionary = new Dictionary<char, int>();
-        //    int counter = 1;
-        //    var allLowerCaseString = str.ToLower();
-        //    Console.WriteLine(allLowerCaseString);
-        //    for (var i = 0; i < allLowerCaseString.Length; i++)
-        //    {
-        //        if (duplicateCounterDictionary.ContainsKey(allLowerCaseString[i]))
-        //        {
-        //            duplicateCounterDictionary[allLowerCaseString[i]]++;
-        //        }
-        //        else
-        //        {
-        //            duplicateCounterDictionary.Add(allLowerCaseString[i], counter);
-        //        }
-        //    }
-        //    return duplicateCounterDictionary.Count(pair => pair.Value > 1);
-        //}
-        #endregion
+    //public static int CountTheDuplicates(string str)
+    //{
+    //    Dictionary<char, int> duplicateCounterDictionary = new Dictionary<char, int>();
+    //    int counter = 1;
+    //    var allLowerCaseString = str.ToLower();
+    //    Console.WriteLine(allLowerCaseString);
+    //    for (var i = 0; i < allLowerCaseString.Length; i++)
+    //    {
+    //        if (duplicateCounterDictionary.ContainsKey(allLowerCaseString[i]))
+    //        {
+    //            duplicateCounterDictionary[allLowerCaseString[i]]++;
+    //        }
+    //        else
+    //        {
+    //            duplicateCounterDictionary.Add(allLowerCaseString[i], counter);
+    //        }
+    //    }
+    //    return duplicateCounterDictionary.Count(pair => pair.Value > 1);
+    //}
+    #endregion
         #region findDiff
 
 
-        //Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
-        //It should remove all values from list a, which are present in list b.
-        //Kata.ArrayDiff(new int[] {1, 2}, new int[] {1}) => new int[] { 2 } 
-        //If a value is present in b, all of its occurrences must be removed from the other:
-        //Kata.ArrayDiff(new int[] { 1, 2, 2, 2, 3 }, new int[] { 2 }) => new int[] { 1, 3 }
+//Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+//It should remove all values from list a, which are present in list b.
+//Kata.ArrayDiff(new int[] {1, 2}, new int[] {1}) => new int[] { 2 } 
+//If a value is present in b, all of its occurrences must be removed from the other:
+//Kata.ArrayDiff(new int[] { 1, 2, 2, 2, 3 }, new int[] { 2 }) => new int[] { 1, 3 }
 
-        public static int[] ArrayDiff(int[] a, int[] b)
+public static int[] ArrayDiff(int[] a, int[] b)
         {
             var makeArrayAList = a.ToList();
             var makeArrayBList = b.ToList();
@@ -135,42 +139,127 @@ namespace TwoSum
         }
 
         #endregion
+        #region romanNumerals 
+        ///**
+        // *Create a function taking a positive integer as its parameter and returning a string containing the Roman Numeral representation of that integer.
+        // * Modern Roman numerals are written by expressing each digit separately starting with the left most digit and skipping any digit with a value of zero.
+        // * In Roman numerals 1990 is rendered: 1000=M, 900=CM, 90=XC; resulting in MCMXC. 2008 is written as 2000=MM, 8=VIII; or MMVIII. 1666 uses each Roman symbol in descending order: MDCLXVI.
+        // * Example:
+        // * RomanConvert.Solution(1000) -- should return "M"
+        // * Help:
+        // * Symbol    Value
+        // * I          1
+        // * V          5
+        // * X          10
+        // * L          50
+        // * C          100
+        // * D          500
+        // * M          1,000
+        // * Remember that there can't be more than 3 identical symbols in a row.
+        // * More about roman numerals
+        // */
 
-        /**
-         *Create a function taking a positive integer as its parameter and returning a string containing the Roman Numeral representation of that integer.
-         * Modern Roman numerals are written by expressing each digit separately starting with the left most digit and skipping any digit with a value of zero.
-         * In Roman numerals 1990 is rendered: 1000=M, 900=CM, 90=XC; resulting in MCMXC. 2008 is written as 2000=MM, 8=VIII; or MMVIII. 1666 uses each Roman symbol in descending order: MDCLXVI.
-         * Example:
-         * RomanConvert.Solution(1000) -- should return "M"
-         * Help:
-         * Symbol    Value
-         * I          1
-         * V          5
-         * X          10
-         * L          50
-         * C          100
-         * D          500
-         * M          1,000
-         * Remember that there can't be more than 3 identical symbols in a row.
-         * More about roman numerals - http://en.wikipedia.org/wiki/Roman_numerals
-         */
+
+
+        //public static string Solution(int number)
+        //{
+        //    List<string> romanNumerals = new List<string>() { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+        //    List<int> numerals = new List<int>() { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+        //    var romanNumeral = string.Empty;
+        //    while (number > 0)
+        //    {
+        //        var index = numerals.FindIndex(x => x <= number);
+
+        //        romanNumeral += romanNumerals[index];
+        //        number -= numerals[index];
+        //    }
+        //    return romanNumeral;
+        //}
+
+        #endregion
+        #region Bouncingball
+        //public static int BouncingBall(double h, double bounce, double window)
+        //{
+        //    var amount = 0;
+        //    if (!(h > 0) || !(bounce > 0 && bounce < 1) || !(h > window)) return -1;
+        //    amount++;
+        //    while (h > 1)
+        //    {
+        //        h *= bounce;
+        //        if (!(h > window)) continue;
+        //        amount += 2;
+
+        //    }
+        //    return amount;
+        //}
 
 
 
-        public static string Solution(int number)
+
+        //A child is playing with a ball on the nth floor of a tall building.The height of this floor, h, is known.
+
+        //    He drops the ball out of the window.The ball bounces (for example), to two-thirds of its height(a bounce of 0.66).
+
+        //His mother looks out of a window 1.5 meters from the ground.
+
+        //    How many times will the mother see the ball pass in front of her window (including when it's falling and bouncing?
+
+        //    Three conditions must be met for a valid experiment:
+        //Float parameter "h" in meters must be greater than 0
+        //Float parameter "bounce" must be greater than 0 and less than 1
+        //Float parameter "window" must be less than h.
+        //    If all three conditions above are fulfilled, return a positive integer, otherwise return -1.
+
+        //Note:
+        //The ball can only be seen if the height of the rebounding ball is strictly greater than the window parameter.
+
+        //    Example:
+        //-
+        //h = 3, bounce = 0.66, window = 1.5, result is 3
+
+        //- h = 3, bounce = 1, window = 1.5, result is -1 
+
+        //(Condition 2) not fulfilled).
+
+        // 10 = h
+        // 10 - 10 /
+
+        //                   var res = BouncingBall(3, 0.66, 1.5);
+        #endregion
+
+        #region TwoSum
+
+        //Write a function that takes an array of numbers(integers for the tests) and a target number.
+        //It should find two different items in the array that, when added together, give the target value. 
+        //The indices of these items should then be returned in a tuple like so: (index1, index2).
+        //For the purposes of this kata, some tests may have multiple answers; any valid solutions will be accepted.
+
+        //The input will always be valid (numbers will be an array of length 2 or greater, and all of the items will
+        //be numbers; target will always be the sum of two different items from that array).
+        //Based on: http://oj.leetcode.com/problems/two-sum/
+
+        public static int[] TwoSum(int[] numbers, int target)
         {
-            List<string> romanNumerals = new List<string>() { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
-            List<int> numerals = new List<int>() { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
-            var romanNumeral = string.Empty;
-            while (number > 0)
+            var previousNumbers = new Dictionary<int, int>();
+             for (var i = 0; i < numbers.Length; i++)
             {
-                var index = numerals.FindIndex(x => x <= number);
+                var difference = target - numbers[i];
 
-                romanNumeral += romanNumerals[index];
-                number -= numerals[index];
+                if (previousNumbers.ContainsKey(difference))
+                {
+                    return new[] { i, previousNumbers[difference] };
+                }
+
+                if (!previousNumbers.ContainsKey(numbers[i]))
+                {
+                    previousNumbers.Add(numbers[i], i);
+                }
             }
-            return romanNumeral;
+            return new int[0];
         }
 
+
+
+        #endregion
     }
 }
